d1a9a86b9b5ca22cb47acfa6c533333a0bf837b4
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2023-07-21 11:18:55 +0700

    create-gui

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -14,13 +14,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView courseName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        courseName = findViewById(R.id.course_name);
-        courseName.setText(R.string.course_name); // Get value of course_name in strings.xml
+
     }
 }
