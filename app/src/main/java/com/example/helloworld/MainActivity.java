68e92087f15549d5b30dee3e8584ca7849930406
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2023-07-28 09:51:10 +0700

    add-date-picker

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.helloworld;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.graphics.Color;
@@ -14,6 +20,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -21,54 +28,81 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import java.time.LocalDate;
 import java.util.Random;
 
-//public class MainActivity extends AppCompatActivity {
-//    // Create an array with options
-//    private String [] workStatus = {"Employed", "Unemployed"};
-//    Spinner sp;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        // Get reference to spinner
-//        sp = findViewById(R.id.spinner);
-//
-//        // Create an adapter
-//        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-//                android.R.layout.simple_spinner_item, workStatus);
-//        // Connect adapter to spinner
-//        sp.setAdapter(dataAdapter);
-//    }
-//
-//}
-
-public class MainActivity extends ListActivity {
-    private static final String[] WEEKDAYS = new String[]{
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday"
-    };
+public class MainActivity extends AppCompatActivity {
+    // DatePicker Fragment inside MainActivity
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState)
+        {
+            LocalDate d = LocalDate.now();
+            int year = d.getYear();
+            int month = d.getMonthValue();
+            int day = d.getDayOfMonth();
+            return new DatePickerDialog(getActivity(), this, year, --month, day);}
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day){
+            LocalDate dob = LocalDate.of(year, ++month, day);
+            ((MainActivity)getActivity()).updateDOB(dob);
+        }
+    }
+    // Create an array with options
+    private String [] workStatus = {"Employed", "Unemployed"};
+    Spinner sp;
+    TextView dobControl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, WEEKDAYS));
-        ListView lv = getListView();
-        lv.setTextFilterEnabled(true);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        setContentView(R.layout.activity_main);
+        dobControl = findViewById(R.id.dob_control);
+
+        dobControl.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long ig) {
-                Toast.makeText(getApplicationContext(),
-                        ((TextView)view).getText(), Toast.LENGTH_SHORT
-                ).show();
+            public void onClick(View view) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
             }
         });
     }
+    public void updateDOB(LocalDate dob){
+        TextView dobControl = findViewById(R.id.dob_control);
+        dobControl.setText(dob.toString());
+    }
+
+
 }
+
+//public class MainActivity extends ListActivity {
+//    private static final String[] WEEKDAYS = new String[]{
+//            "Monday",
+//            "Tuesday",
+//            "Wednesday",
+//            "Thursday",
+//            "Friday",
+//            "Saturday",
+//            "Sunday"
+//    };
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        //setContentView(R.layout.activity_main);
+//        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, WEEKDAYS));
+//        ListView lv = getListView();
+//        lv.setTextFilterEnabled(true);
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long ig) {
+//                Toast.makeText(getApplicationContext(),
+//                        ((TextView)view).getText(), Toast.LENGTH_SHORT
+//                ).show();
+//            }
+//        });
+//    }
+//}
+
+
 
