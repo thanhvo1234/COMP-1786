c8b6f460c4f60f70a0cf437470009e2dd5e78c14
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2023-07-28 09:27:24 +0700

    populate-spinner-through-code

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -10,8 +10,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,42 +21,21 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar myToolBar;
-
+    // Create an array with options
+    private String [] workStatus = {"Employed", "Unemployed"};
+    Spinner sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Get a reference from the toolbar
-        myToolBar = findViewById(R.id.toolbar);
 
-// Set toolbar as actionbar for the activity
-        setSupportActionBar(myToolBar);
-        }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        if (item.getItemId() == R.id.itemNext){
-            getInputs();
-            return true;
-        }
-        else if (item.getItemId() == R.id.itemExit){
-            Toast.makeText(
-                    getApplicationContext(),
-                    "You asked to exit, but why not start another app?",
-                    Toast.LENGTH_LONG
-            ).show();
-            return true;
-        }
-        else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
+        // Get reference to spinner
+        sp = findViewById(R.id.spinner);
 
-    private void getInputs() {
+        // Create an adapter
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, workStatus);
+        // Connect adapter to spinner
+        sp.setAdapter(dataAdapter);
     }
 }
