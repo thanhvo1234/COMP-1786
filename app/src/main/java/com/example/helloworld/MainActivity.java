85e1f50b8edd88387195f70a1afb5b6c38c0a923
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2023-07-28 09:58:08 +0700

    checking-the-checkbox

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -8,9 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -20,6 +22,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -50,9 +53,53 @@ public class MainActivity extends AppCompatActivity {
             ((MainActivity)getActivity()).updateDOB(dob);
         }
     }
-    // Create an array with options
-    private String [] workStatus = {"Employed", "Unemployed"};
+
+
+    public  void updateDOB(LocalDate dob){
+        TextView dobControl = findViewById(R.id.dob_control);
+        dobControl.setText(dob.toString());
+    }
+    // Define variables to reference the layout
+    EditText nameInput;
+    EditText emailInput;
+    EditText phoneInput;
     Spinner sp;
+    String workStatus;
+    Button submitBtn;
+
+    // Define function getInputs() to get values from inputs
+    private void getInputs(){
+        nameInput = findViewById(R.id.name_input);
+        emailInput = findViewById(R.id.email_input);
+        phoneInput = findViewById(R.id.phone_input);
+
+        String name = nameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String phone = phoneInput.getText().toString();
+        workStatus = sp.getSelectedItem().toString();
+
+        // Use function displayNextAlert to display an AlertDialog
+        displayNextAlert(name, phone, email, workStatus);
+    }
+
+    public void displayNextAlert(String name, String phone, String email, String workStatus){
+        new AlertDialog.Builder(this)
+                .setTitle("Details Entered")
+                .setMessage(
+                        "Details: \n" +
+                                name + "\n" +
+                                phone + "\n" +
+                                email + "\n" +
+                                workStatus
+                )
+                .setNeutralButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .show();
+    }
     TextView dobControl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,10 +114,27 @@ public class MainActivity extends AppCompatActivity {
                 newFragment.show(getSupportFragmentManager(), "datePicker");
             }
         });
-    }
-    public void updateDOB(LocalDate dob){
-        TextView dobControl = findViewById(R.id.dob_control);
-        dobControl.setText(dob.toString());
+
+        sp = findViewById(R.id.spinner);
+        submitBtn = findViewById(R.id.submit_btn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CheckBox cb = findViewById(R.id.checkBox);
+
+                if (!cb.isChecked()){
+                    Toast
+                            .makeText(
+                                    MainActivity.this,
+                                    "You must check the box",
+                                    Toast.LENGTH_LONG
+                            )
+                            .show();
+                    return;
+                }
+                getInputs();
+            }
+        });
     }
 
 
